7209ba77d5017f0063ecec6770f810c38c3c0845
Muthusamy R <muthusamy.ramalingam@amd.com>
2024-06-27 15:30:31 +0530

    default error handling

--- src/collect/cpustat.go
+++ src/collect/cpustat.go
@@ -42,32 +42,69 @@ package collect
 import "github.com/MuthusamyRamalingam/go_amd_smi"
 //import "github.com/amd/go_amd_smi"
 
+var MAX_CPU_SOCKET = 4
+var MAX_CPU_THREADS = 768
+var MAX_GPU = 24
+
 type AMDParams struct {
-	CoreEnergy [768]float64
-	SocketEnergy [4]float64
-	CoreBoost [768]float64
-	SocketPower [4]float64
-	PowerLimit [4]float64
-	ProchotStatus [4]float64
+	CoreEnergy [MAX_CPU_THREADS]float64
+	SocketEnergy [MAX_CPU_SOCKET]float64
+	CoreBoost [MAX_CPU_THREADS]float64
+	SocketPower [MAX_CPU_SOCKET]float64
+	PowerLimit [MAX_CPU_SOCKET]float64
+	ProchotStatus [MAX_CPU_SOCKET]float64
 	Sockets uint
 	Threads uint
 	ThreadsPerCore uint
 	NumGPUs uint
-	GPUDevId [24]float64
-	GPUPowerCap [24]float64
-	GPUPowerAvg [24]float64
-	GPUTemperature [24]float64
-	GPUSCLK [24]float64
-	GPUMCLK [24]float64
-	GPUUsage [24]float64
-	GPUMemoryUsage [24]float64
+	GPUDevId [MAX_GPU]float64
+	GPUPowerCap [MAX_GPU]float64
+	GPUPowerAvg [MAX_GPU]float64
+	GPUTemperature [MAX_GPU]float64
+	GPUSCLK [MAX_GPU]float64
+	GPUMCLK [MAX_GPU]float64
+	GPUUsage [MAX_GPU]float64
+	GPUMemoryUsage [MAX_GPU]float64
+}
+
+func (amdParams *AMDParams) Init() {
+
+	amdParams.Sockets = -1
+	amdParams.Threads = -1
+	amdParams.ThreadsPerCore = -1
+	amdParams.NumGPUs = -1
+
+	for socketloopCounter := 0; socketloopCounter < MAX_CPU_SOCKET; socketloopCounter++	{ 
+		amdParams.SocketEnergy = -1
+		amdParams.SocketPower = -1
+		amdParams.PowerLimit = -1
+		amdParams.ProchotStatus = -1
+	}
+	
+	for logicalCoreloopCounter := 0; logicalCoreloopCounter < MAX_CPU_THREADS; logicalCoreloopCounter++	{ 
+		amdParams.CoreEnergy = -1
+		amdParams.CoreBoost = -1
+	}
+	
+	for gpuloopCounter := 0; gpuloopCounter < MAX_GPU; gpuloopCounter++	{ 
+		amdParams.GPUDevId = -1
+		amdParams.GPUPowerCap = -1
+		amdParams.GPUPowerAvg = -1
+		amdParams.GPUTemperature = -1
+		amdParams.GPUSCLK = -1
+		amdParams.GPUMCLK = -1
+		amdParams.GPUUsage = -1
+		amdParams.GPUMemoryUsage = -1
+	}
 }
 
 func Scan() (AMDParams) {
 
 	var stat AMDParams
-	value64 := uint64(0)
-	value32 := uint32(0)
+	stat.Init()
+	
+	value64 := int(0)
+	value32 := int(0)
 	
 	value16 := uint16(0)
 
@@ -81,29 +118,29 @@ func Scan() (AMDParams) {
 		stat.ThreadsPerCore = uint(num_threads_per_core)
 
 		for i := 0; i < num_threads ; i++ {
-			value64 = uint64(goamdsmi.GO_cpu_core_energy_get(i))
+			value64 = int(goamdsmi.GO_cpu_core_energy_get(i))
 			stat.CoreEnergy[i] = float64(value64)
 			value64 = 0
 
-			value32 = uint32(goamdsmi.GO_cpu_core_boostlimit_get(i))
+			value32 = int(goamdsmi.GO_cpu_core_boostlimit_get(i))
 			stat.CoreBoost[i] = float64(value32)
 			value32 = 0
 		}
 
 		for i := 0; i < num_sockets ; i++ {
-			value64 = uint64(goamdsmi.GO_cpu_socket_energy_get(i))
+			value64 = int(goamdsmi.GO_cpu_socket_energy_get(i))
 			stat.SocketEnergy[i] = float64(value64)
 			value64 = 0
 
-			value32 = uint32(goamdsmi.GO_cpu_socket_power_get(i))
+			value32 = int(goamdsmi.GO_cpu_socket_power_get(i))
 			stat.SocketPower[i] = float64(value32)
 			value32 = 0
 
-			value32 = uint32(goamdsmi.GO_cpu_socket_power_cap_get(i))
+			value32 = int(goamdsmi.GO_cpu_socket_power_cap_get(i))
 			stat.PowerLimit[i] = float64(value32)
 			value32 = 0
 
-			value32 = uint32(goamdsmi.GO_cpu_prochot_status_get(i))
+			value32 = int(goamdsmi.GO_cpu_prochot_status_get(i))
 			stat.ProchotStatus[i] = float64(value32)
 			value32 = 0
 		}
@@ -115,36 +152,36 @@ func Scan() (AMDParams) {
 		stat.NumGPUs = uint(num_gpus)
 
 		for i := 0; i < num_gpus ; i++ {
-			value16 = uint16(goamdsmi.GO_gpu_dev_id_get(i))
+			value16 = int(goamdsmi.GO_gpu_dev_id_get(i))
 			stat.GPUDevId[i] = float64(value16)
 			value16 = 0
 
-			value64 = uint64(goamdsmi.GO_gpu_dev_power_cap_get(i))
+			value64 = int(goamdsmi.GO_gpu_dev_power_cap_get(i))
 			stat.GPUPowerCap[i] = float64(value64)
 			value64 = 0
 
-			value64 = uint64(goamdsmi.GO_gpu_dev_power_ave_get(i))
+			value64 = int(goamdsmi.GO_gpu_dev_power_ave_get(i))
 			stat.GPUPowerAvg[i] = float64(value64)
 			value64 = 0
 
 			//Get the value for GPU current temperature. Sensor = 0(GPU), Metric = 0(current)
-			value64 = uint64(goamdsmi.GO_gpu_dev_temp_metric_get(i, 0, 0))
+			value64 = int(goamdsmi.GO_gpu_dev_temp_metric_get(i, 0, 0))
 			stat.GPUTemperature[i] = float64(value64)
 			value64 = 0
 
-			value64 = uint64(goamdsmi.GO_gpu_dev_gpu_clk_freq_get_sclk(i))
+			value64 = int(goamdsmi.GO_gpu_dev_gpu_clk_freq_get_sclk(i))
 			stat.GPUSCLK[i] = float64(value64)
 			value64 = 0
 
-			value64 = uint64(goamdsmi.GO_gpu_dev_gpu_clk_freq_get_mclk(i))
+			value64 = int(goamdsmi.GO_gpu_dev_gpu_clk_freq_get_mclk(i))
 			stat.GPUMCLK[i] = float64(value64)
 			value64 = 0
 
-			value64 = uint64(goamdsmi.GO_gpu_dev_gpu_busy_percent_get(i))
+			value64 = int(goamdsmi.GO_gpu_dev_gpu_busy_percent_get(i))
 			stat.GPUUsage[i] = float64(value64)
 			value64 = 0
 
-			value64 = uint64(goamdsmi.GO_gpu_dev_gpu_memory_busy_percent_get(i))
+			value64 = int(goamdsmi.GO_gpu_dev_gpu_memory_busy_percent_get(i))
 			stat.GPUMemoryUsage[i] = float64(value64)
 			value64 = 0
 		}
